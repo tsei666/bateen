96fc6a3b4fcc841ff1c36baaa5624824fa586b74
Tim Seitz <tim@seitz-bonn.de>
2025-03-15 16:00:53 +0100

    Init Spring

---
+++ src/test/java/de/breuer/bateen/BateenApplicationTests.java
@@ -0,0 +1,13 @@
+package de.breuer.bateen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BateenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
